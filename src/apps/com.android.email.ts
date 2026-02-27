870bf50ec770f69807c028e1900f364d1252c875
天方夜谭 <tianfangyetan01@qq.com>
2026-02-27 23:20:41 +0800

    feat: 小米邮件-非合并账户自动切换到合并账户

--- src/apps/com.android.email.ts
+++ src/apps/com.android.email.ts
@@ -25,5 +25,32 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '功能类-【小米】非合并账户自动切换到合并账户',
+      desc: '仅在收件箱界面开屏时触发',
+      rules: [
+        {
+          key: 0,
+          fastQuery: true,
+          matchTime: 5000,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          activityIds: 'com.wps.multiwindow.main.HomeActivity',
+          matches:
+            '@[vid="titleContainer"][clickable=true] > [text!="合并账户"] - [text="收件箱"]',
+          snapshotUrls: 'https://i.gkd.li/i/25607057',
+        },
+        {
+          key: 1,
+          preKeys: 0,
+          fastQuery: true,
+          actionDelay: 200, // 目标节点不是 clickable 的，等动画放完再点
+          activityIds: 'com.wps.multiwindow.main.HomeActivity',
+          matches: '[text="合并账户"][checkable=true]',
+          snapshotUrls: 'https://i.gkd.li/i/25607113',
+        },
+      ],
+    },
   ],
 });
